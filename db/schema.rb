6ff51fb5536b3334f8fdb5fcc561c7962293af14
leon.dahmer <leon.dahmer@gmail.com>
2018-05-12 15:42:11 -0400

    Added groups migration and first model version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_12_130547) do
+ActiveRecord::Schema.define(version: 2018_05_12_193502) do
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
