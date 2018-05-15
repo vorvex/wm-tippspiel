dcdaff9b5cb3a56d56f6e6e867b33e291aa2eeb5
leon.dahmer <leon.dahmer@gmail.com>
2018-05-15 17:32:13 -0400

    Nickname Registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,26 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => {email: true, login: false}
   
   validates :nickname, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
   
   has_many :tipps
+  
+  attr_writer :login
+
+  def login
+    @login || self.username || self.email
+  end
+  
+  def self.find_for_database_authentication(warden_conditions)
+      conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions.to_h).where(["lower(nickname) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+        where(conditions.to_h).first
+      end
+    end
   
   def position()
     x = User.where('points > ?', self.points).count + 1
