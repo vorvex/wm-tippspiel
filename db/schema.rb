29da88a4e8df3450ceea29adf1a94ea3f714310f
leon.dahmer <leon.dahmer@gmail.com>
2018-05-22 08:30:59 -0400

    Confirmable added

--- db/schema.rb
+++ db/schema.rb
@@ -95,6 +95,10 @@ ActiveRecord::Schema.define(version: 2018_05_20_204253) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.string "role_name", default: "guest"
     t.integer "points", default: 0
     t.string "nickname", null: false
